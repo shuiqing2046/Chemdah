7ce5d0d8bd9de3e3d3b18f2ae9cff43afcb4ba8d
Peng_Lx <L834787411@outlook.com>
2021-05-29 21:37:58 +0800

    update

--- src/main/java/ink/ptms/chemdah/core/quest/objective/skillapi/SSkillUnlock.kt
+++ src/main/java/ink/ptms/chemdah/core/quest/objective/skillapi/SSkillUnlock.kt
@@ -1,4 +1,28 @@
 package ink.ptms.chemdah.core.quest.objective.skillapi
 
-object SSkillUnlock {
+import ink.ptms.chemdah.core.quest.objective.Dependency
+import com.sucy.skill.api.event.PlayerSkillUnlockEvent
+import ink.ptms.chemdah.core.quest.objective.ObjectiveCountableI
+
+/**
+ * Chemdah
+ * ink.ptms.chemdah.core.quest.objiective.skillapi.SSkillUnlock
+ *
+ * @author Peng_Lx
+ * @since 2021/5/29 7:59 下午
+ */
+@Dependency("SkillAPI")
+object SSkillUnlock : ObjectiveCountableI<PlayerSkillUnlockEvent>() {
+
+    override val name = "skillapi skillunlock"
+    override val event = PlayerSkillUnlockEvent::class
+
+    init {
+        handler {
+            playerData.player
+        }
+        addCondition("skill") {
+            toString().equals(it.unlockedSkill.status.name, true)
+        }
+    }
 }
