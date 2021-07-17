8da3b141e51a089c4aafe1394f7878141121d895
GalaxyVN <52485153+Galaxy-VN@users.noreply.github.com>
2021-07-17 15:54:10 +0700

    add back citizens wrong dir and add some chat ?

---
+++ src/main/java/ink/ptms/chemdah/core/quest/objective/citizens/CNPCInteract.kt
@@ -0,0 +1,30 @@
+package ink.ptms.chemdah.core.quest.objective.citizens
+
+import ink.ptms.chemdah.core.quest.objective.Dependency
+import ink.ptms.chemdah.core.quest.objective.ObjectiveCountableI
+import net.citizensnpcs.api.event.NPCRightClickEvent
+
+@Dependency("Citizens")
+object CNPCInteract : ObjectiveCountableI<NPCRightClickEvent>() {
+
+    override val name = "cnpc interact"
+    override val event = NPCRightClickEvent::class
+
+    init {
+        handler {
+            clicker
+        }
+        addCondition("position") { e ->
+            toPosition().inside(e.npc.entity.location)
+        }
+        addCondition("name") { e ->
+            asList().any { it.equals(e.npc.name, true) }
+        }
+        addCondition("type") { e ->
+            asList().any { it.equals(e.npc.entity.type.name, true) }
+        }
+        addConditionVariable("name") {
+            it.npc.name
+        }
+    }
+}
