cd6447227acb322ce0ce928534c8f4a55afdd769
GalaxyVN <52485153+Galaxy-VN@users.noreply.github.com>
2021-07-18 17:16:02 +0700

    Update CNPCDamage.kt

--- src/main/java/ink/ptms/chemdah/core/quest/objective/citizens/CNPCDamage.kt
+++ src/main/java/ink/ptms/chemdah/core/quest/objective/citizens/CNPCDamage.kt
@@ -2,43 +2,35 @@ package ink.ptms.chemdah.core.quest.objective.citizens
 
 import ink.ptms.chemdah.core.quest.objective.Dependency
 import ink.ptms.chemdah.core.quest.objective.ObjectiveCountableI
-import ink.ptms.chemdah.core.quest.objective.adyeshach.ANPCDamage
-import net.citizensnpcs.api.event.NPCDamageByEntityEvent
-import org.bukkit.entity.Player
+import net.citizensnpcs.api.event.NPCRightClickEvent
 
 @Dependency("Citizens")
-object CNPCDamage : ObjectiveCountableI<NPCDamageByEntityEvent>() {
+object CNPCInteract : ObjectiveCountableI<NPCRightClickEvent>() {
 
-    override val name = "cnpc damage"
-    override val event = NPCDamageByEntityEvent::class
+    override val name = "cnpc interact"
+    override val event = NPCRightClickEvent::class
 
     init {
         handler {
-            damager as? Player
+            clicker
         }
         addCondition("position") { e ->
-            toPosition().inside(e.damager.location)
-        }
-        addCondition("name") { e ->
-            asList().any { it.equals(e.npc.name, true) }
+            toPosition().inside(e.npc.entity.location)
         }
         addCondition("id") { e ->
             toInt() == e.npc.id
         }
+        addCondition("name") { e ->
+            asList().any { it.equals(e.npc.name, true)}
+        }
         addCondition("type") { e ->
             asList().any { it.equals(e.npc.entity.type.name, true) }
         }
-        addCondition("damage") {
-            toInt() <= it.damage
+        addConditionVariable("id") {
+            it.npc.id
         }
         addConditionVariable("name") {
             it.npc.name
-        }
-        addConditionVariable("damage") {
-            it.damage
-        }
-        addConditionVariable("id") {
-            it.npc.id
         }
     }
 }
