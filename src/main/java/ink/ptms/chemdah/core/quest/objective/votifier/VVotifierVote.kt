d3d4dfd8aa708d5320e40493cc9410ff7f78fbdc
GalaxyVN <52485153+Galaxy-VN@users.noreply.github.com>
2021-07-19 15:39:35 +0700

    Just vote...

---
+++ src/main/java/ink/ptms/chemdah/core/quest/objective/votifier/VVotifierVote.kt
@@ -0,0 +1,22 @@
+package ink.ptms.chemdah.core.quest.objective.votifier
+
+import ink.ptms.chemdah.core.quest.objective.Dependency
+import ink.ptms.chemdah.core.quest.objective.ObjectiveCountableI
+import com.vexsoftware.votifier.model.VotifierEvent
+import org.bukkit.Bukkit
+
+@Dependency("votifier")
+object VVotifierVote : ObjectiveCountableI<VotifierEvent>() {
+
+    override val name = "votifier vote"
+    override val event = VotifierEvent::class
+
+    init {
+        handler {
+            Bukkit.getPlayer(vote.username)
+        }
+        addCondition("position") {
+            toPosition().inside(Bukkit.getPlayer(it.vote.username)!!.location)
+        }
+    }
+}
