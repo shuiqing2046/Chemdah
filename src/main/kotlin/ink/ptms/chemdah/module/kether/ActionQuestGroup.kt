0bd6825610ba7fe41116d78a3bdc6b1581554766
坏黑 <b@skymc.me>
2021-11-02 19:24:30 +0800

    [0.1.7] Update "quest-group" action

---
+++ src/main/kotlin/ink/ptms/chemdah/module/kether/ActionQuestGroup.kt
@@ -0,0 +1,53 @@
+package ink.ptms.chemdah.module.kether
+
+import ink.ptms.chemdah.api.ChemdahAPI
+import ink.ptms.chemdah.util.getProfile
+import taboolib.library.kether.ArgTypes
+import taboolib.module.kether.*
+
+/**
+ * Chemdah
+ * ink.ptms.chemdah.module.kether.ActionQuestGroup
+ *
+ * @author mac
+ * @since 2021/11/2 7:17 下午
+ */
+object ActionQuestGroup {
+
+    /**
+     * quest-group [accepted|completed] *group
+     */
+    @KetherParser(["quest-group"], shared = true)
+    fun parser1() = scriptParser {
+        it.switch {
+            case("accepted") {
+                val quest = it.next(ArgTypes.ACTION)
+                actionFuture { future ->
+                    newFrame(quest).run<Any>().thenApply { quest ->
+                        val templateGroup = ChemdahAPI.getQuestTemplateGroup(quest.toString())
+                        if (templateGroup == null) {
+                            future.complete(false)
+                        } else {
+                            val profile = getProfile()
+                            future.complete(templateGroup.group.all { t -> profile.getQuestById(t.id) != null })
+                        }
+                    }
+                }
+            }
+            case("completed") {
+                val quest = it.next(ArgTypes.ACTION)
+                actionFuture { future ->
+                    newFrame(quest).run<Any>().thenApply { quest ->
+                        val templateGroup = ChemdahAPI.getQuestTemplateGroup(quest.toString())
+                        if (templateGroup == null) {
+                            future.complete(false)
+                        } else {
+                            val profile = getProfile()
+                            future.complete(templateGroup.group.all { t -> profile.isQuestCompleted(t) })
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
