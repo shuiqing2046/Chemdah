db20ee4c27189c0722191c9ac04d62b665293742
坏黑 <b@skymc.me>
2022-08-16 12:19:54 +0800

    [0.2.30] Fix loader

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ taboolib {
     install("expansion-command-helper")
     install("expansion-javascript")
     classifier = null
-    version = "6.0.9-59"
+    version = "6.0.9-62"
     relocate("ink.ptms.um", "ink.ptms.chemdah.um")
 }
 
