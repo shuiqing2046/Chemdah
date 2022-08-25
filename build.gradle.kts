1c0b721e5e8c958efb3e5314ce3063040eed635c
坏黑 <b@skymc.me>
2022-08-25 17:35:36 +0800

    [0.2.33] Fix Flags

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ taboolib {
     install("expansion-command-helper")
     install("expansion-javascript")
     classifier = null
-    version = "6.0.9-65"
+    version = "6.0.9-71"
     relocate("ink.ptms.um", "ink.ptms.chemdah.um")
 }
 
