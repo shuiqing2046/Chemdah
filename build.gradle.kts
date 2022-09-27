fef41ed911839df5c3f66e5d18441dee055273da
坏黑 <b@skymc.me>
2022-09-27 08:14:41 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ taboolib {
     install("expansion-command-helper")
     install("expansion-javascript")
     classifier = null
-    version = "6.0.9-97"
+    version = "6.0.9-99"
     relocate("ink.ptms.um", "ink.ptms.chemdah.um")
 }
 
